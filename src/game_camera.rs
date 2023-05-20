965a0ca72596ccf27fce31a79bbb8bb56144a3c5
Jerome <jerome.f.bollinger@gmail.com>
2023-05-20 17:13:12 +0200

    refactor(code): formatting

--- src/game_camera.rs
+++ src/game_camera.rs
@@ -11,26 +11,22 @@ pub fn default_camera(mut commands: Commands) {
 pub fn camera_zoom(
     kb: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<&mut OrthographicProjection, With<Camera>>) {
-
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+) {
     let dist = ZOOM_SPEED * time.delta().as_secs_f32();
-
     for mut projection in query.iter_mut() {
         let mut log_scale = projection.scale.ln();
-
         if kb.pressed(KeyCode::PageUp) {
             if log_scale - dist > ZOOM_MAX {
                 log_scale -= dist;
-            }
-            else {
+            } else {
                 log_scale = ZOOM_MAX;
             }
         }
         if kb.pressed(KeyCode::PageDown) {
             if log_scale + dist < ZOOM_MIN {
                 log_scale += dist;
-            }
-            else {
+            } else {
                 log_scale = ZOOM_MIN;
             }
         }
