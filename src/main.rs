8e2948f5a0d19eeb45b229d6b39e61fb7d8b2be0
Jerome Bollinger <67476972+JeromeBollinger@users.noreply.github.com>
2023-05-20 16:39:58 +0200

    feat(code): [#1] Introduce bevy game engine to project (#2)
    * feat(art): Add player image
    
    * feat(code): Introduce bevy game engine to project
    
    * feat(code): Add scene camera to window
    
    * feat(code): Add player mechanics to game
    
    * refactor(code): Formatter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use crate::game_camera::*;
+use crate::player::*;
+use bevy::prelude::*;
+
+mod game_camera;
+mod player;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Idle Colony Game".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(default_camera)
+        .add_startup_system(spawn_player)
+        .add_system(player_movement)
+        .add_system(exit_game)
+        .run();
 }
