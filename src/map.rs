d863ad86d76db8a9cfe9c721bebb646287790257
Jerome <jerome.f.bollinger@gmail.com>
2023-05-22 21:58:23 +0200

    refactor(map): extract tile placement initiator

--- src/map.rs
+++ src/map.rs
@@ -2,11 +2,34 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 
 pub fn initiate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let texture_handle: Handle<Image> = asset_server.load("regolith.png");
+    let texture_handle: Vec<Handle<Image>> = vec![
+        asset_server.load("regolith.png"),
+        asset_server.load("regolith2.png"),
+    ];
     let map_size = TilemapSize { x: 32, y: 32 };
+
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    let (tilemap_entity, tile_storage) = create_map(map_size, &mut commands);
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Vector(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+}
+
+fn create_map(map_size: TilemapSize, commands: &mut Commands) -> (Entity, TileStorage) {
     let tilemap_entity = commands.spawn_empty().id();
     let mut tile_storage = TileStorage::empty(map_size);
-
     for x in 0..map_size.x {
         for y in 0..map_size.y {
             let tile_pos = TilePos { x, y };
@@ -20,19 +43,5 @@ pub fn initiate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
             tile_storage.set(&tile_pos, tile_entity);
         }
     }
-
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = tile_size.into();
-    let map_type = TilemapType::default();
-
-    commands.entity(tilemap_entity).insert(TilemapBundle {
-        grid_size,
-        map_type,
-        size: map_size,
-        storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
-        tile_size,
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
-        ..Default::default()
-    });
+    (tilemap_entity, tile_storage)
 }
