3282cead42a74984b6bf664361564451e30be11b
Jerome Bollinger <67476972+JeromeBollinger@users.noreply.github.com>
2023-05-29 13:16:08 +0200

    feat(map): [#20] wall collision (#34)
    * feat: Add transparent tile
    
    * refactor: move stuff, rename variable
    
    * feat: Add second asset handle var
    
    * refactor: Pass index map to create_map()
    
    * feat: Add map_floor.txt
    
    * refactor: Specifiy entity and storage var
    
    * feat: Add floor tilemap entity
    
    * fix: Add one to wall and player z-index
    
    * refactor: cargo fmt --all
    
    * feat: Add Solid component
    
    * chore: Use numbers for maps
    
    * chore: Add new structs and enum
    
    * chore: implement new map types
    
    * feat: Add collision detection
    
    * refactor: format
    
    * fix: first clippy warning (introduced hitbox)
    
    * fix: ignore type_complexity warning
    
    * refactor: format

--- src/map.rs
+++ src/map.rs
@@ -4,9 +4,20 @@ use std::fs::File;
 use std::io::*;
 use std::path::Path;
 
+pub const MAP_WIDTH: u32 = 32;
+pub const MAP_HEIGTH: u32 = 32;
+pub const TILE_WIDTH: u32 = 16;
+pub const TILE_HEIGTH: u32 = 16;
+
 pub fn initiate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let map_size = TilemapSize { x: 32, y: 32 };
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let map_size = TilemapSize {
+        x: MAP_WIDTH,
+        y: MAP_HEIGTH,
+    };
+    let tile_size = TilemapTileSize {
+        x: TILE_WIDTH as f32,
+        y: TILE_HEIGTH as f32,
+    };
     let grid_size = tile_size.into();
     let map_type = TilemapType::default();
     let wall_texture_handle: Vec<Handle<Image>> = vec![
@@ -18,24 +29,30 @@ pub fn initiate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
     let (wall_tilemap_entity, wall_tile_storage) = create_map(
         map_size,
         &mut commands,
-        read_map(Path::new("assets/maps/map1.txt")),
+        MapKind::SolidMap(
+            Map::from_file(Path::new("assets/maps/map1.txt")),
+            Map::from_file(Path::new("assets/maps/map1.txt")),
+        ),
     );
     let (floor_tilemap_entity, floor_tile_storage) = create_map(
         map_size,
         &mut commands,
-        read_map(Path::new("assets/maps/map_floor.txt")),
+        MapKind::AssetIndexMap(Map::from_file(Path::new("assets/maps/map_floor.txt"))),
     );
 
-    commands.entity(wall_tilemap_entity).insert(TilemapBundle {
-        grid_size,
-        map_type,
-        size: map_size,
-        storage: wall_tile_storage,
-        texture: TilemapTexture::Vector(wall_texture_handle),
-        tile_size,
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 1.0),
-        ..Default::default()
-    });
+    commands.entity(wall_tilemap_entity).insert((
+        TilemapBundle {
+            grid_size,
+            map_type,
+            size: map_size,
+            storage: wall_tile_storage,
+            texture: TilemapTexture::Vector(wall_texture_handle),
+            tile_size,
+            transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 1.0),
+            ..Default::default()
+        },
+        WallMapComponent {},
+    ));
 
     commands.entity(floor_tilemap_entity).insert(TilemapBundle {
         grid_size,
@@ -49,45 +66,100 @@ pub fn initiate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
+#[derive(Component)]
+pub struct WallMapComponent {}
+
 fn create_map(
     map_size: TilemapSize,
     commands: &mut Commands,
-    index_map: Vec<Vec<u32>>,
+    map: MapKind,
 ) -> (Entity, TileStorage) {
     let tilemap_entity = commands.spawn_empty().id();
     let mut tile_storage = TileStorage::empty(map_size);
-    for x in 0..map_size.x {
-        for y in 0..map_size.y {
-            let tile_pos = TilePos { x, y };
-            let tile_entity = commands
-                .spawn(TileBundle {
-                    position: tile_pos,
-                    tilemap_id: TilemapId(tilemap_entity),
-                    texture_index: TileTextureIndex(index_map[x as usize][y as usize]),
-                    ..Default::default()
-                })
-                .id();
-            tile_storage.set(&tile_pos, tile_entity);
-        }
-    }
-    (tilemap_entity, tile_storage)
-}
-
-fn read_map(map_path: &Path) -> Vec<Vec<u32>> {
-    let input = File::open(map_path).expect("No map found");
-    let mut map: Vec<Vec<u32>> = vec![vec![]];
-
-    for (_, line) in BufReader::new(input).lines().enumerate() {
-        if let Ok(line) = line {
-            for (x, c) in line.chars().enumerate() {
-                match c {
-                    'o' => map[x].push(0),
-                    'x' => map[x].push(1),
-                    _ => map[x].push(1),
+    match map {
+        MapKind::AssetIndexMap(m) => {
+            for x in 0..map_size.x {
+                for y in 0..map_size.y {
+                    let tile_pos = TilePos { x, y };
+                    let tile_entity = commands
+                        .spawn(TileBundle {
+                            position: tile_pos,
+                            tilemap_id: TilemapId(tilemap_entity),
+                            texture_index: TileTextureIndex(m.map()[x as usize][y as usize]),
+                            ..Default::default()
+                        })
+                        .id();
+                    tile_storage.set(&tile_pos, tile_entity);
                 }
-                map.push(vec![]);
+            }
+        }
+        MapKind::SolidMap(tm, sm) => {
+            for x in 0..map_size.x {
+                for y in 0..map_size.y {
+                    let tile_pos = TilePos { x, y };
+                    if sm.map()[x as usize][y as usize] == 0 {
+                        let tile_entity = commands
+                            .spawn((TileBundle {
+                                position: tile_pos,
+                                tilemap_id: TilemapId(tilemap_entity),
+                                texture_index: TileTextureIndex(tm.map()[x as usize][y as usize]),
+                                ..Default::default()
+                            },))
+                            .id();
+                        tile_storage.set(&tile_pos, tile_entity);
+                    } else {
+                        let tile_entity = commands
+                            .spawn((
+                                TileBundle {
+                                    position: tile_pos,
+                                    tilemap_id: TilemapId(tilemap_entity),
+                                    texture_index: TileTextureIndex(
+                                        tm.map()[x as usize][y as usize],
+                                    ),
+                                    ..Default::default()
+                                },
+                                Solid {},
+                            ))
+                            .id();
+                        tile_storage.set(&tile_pos, tile_entity);
+                    }
+                }
             }
         }
     }
-    map
+
+    (tilemap_entity, tile_storage)
+}
+
+#[derive(Component)]
+pub struct Solid {}
+
+struct Map(Vec<Vec<u32>>);
+
+impl Map {
+    fn from_file(map_path: &Path) -> Self {
+        let input = File::open(map_path).expect("No map found");
+        let mut map: Vec<Vec<u32>> = vec![vec![]];
+
+        for (_, line) in BufReader::new(input).lines().enumerate() {
+            if let Ok(line) = line {
+                for (x, c) in line.chars().enumerate() {
+                    match c.to_digit(10) {
+                        Some(i) => map[x].push(i),
+                        None => map[x].push(0),
+                    }
+                    map.push(vec![]);
+                }
+            }
+        }
+        Map(map)
+    }
+    fn map(&self) -> &Vec<Vec<u32>> {
+        &self.0
+    }
+}
+
+enum MapKind {
+    AssetIndexMap(Map),
+    SolidMap(Map, Map),
 }
